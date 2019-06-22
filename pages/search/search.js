605672780948dbf8fc8e38605a8ac1ed5d2c4974
hxfeng <993305280@qq.com>
2019-06-23 06:24:29 +0800

    第二次提交

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,5 +1,7 @@
 // pages/search/search.js
-import { network } from "../../utils/network.js";
+import {
+  network
+} from "../../utils/network.js";
 
 Page({
 
@@ -7,27 +9,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
-    
+  onLoad: function(options) {
+    var that = this
+    wx.getStorage({
+      key: 'historyVideo',
+      success: function(res) {
+        // console.log(res.data)
+        var data = res.data;
+        that.setData({
+          historyArray: data
+        })
+      },
+    })
+
   },
-  onsearchListData:function (event) {
+  onsearchListData: function(event) {
     var that = this
     // console.log(event.detail.value)
     var userSearchInput = event.detail.value;
-    this.setData({
-      userSearchInput: userSearchInput
-    })
+    if (!userSearchInput || userSearchInput === "") {
+      that.setData({
+        searchTotailData: null
+      });
+      return;
+    }
+    // that.setData({
+    //   userSearchInput: userSearchInput
+    // })
     network.getSearchData({
       searchValue: userSearchInput,
-      success: function (res) { 
-        // console.log(res)
+      success: function(res) {
+        // console.log(res.data.subjects)
         var searchTotailData = res.data.subjects
         that.setData({
           searchTotailData: searchTotailData
@@ -35,60 +53,61 @@ Page({
       }
     })
   },
-  onDetailPage:function (event) {
-    console.log(event)
+  onSearchDetailPage: function(event) {
+    var that = this
     var id = event.currentTarget.dataset.id;
+    var title = event.currentTarget.dataset.title;
+    var historyArray = that.data.historyArray;
+   
+    var isEixted = false;
+    if (historyArray) {
+      for (var index=0; index<historyArray.length; index++) {
+        var movies = historyArray[index];
+        console.log(movies)
+        // console.log(id)
+        if(movies.id == id) {
+          console.log(2)
+          isEixted = true;
+          break;
+        }
+      }
+      // console.log(2)
+    }
+    if (!isEixted) {
+      if (!historyArray) {
+        historyArray = [];
+      }
+      historyArray.push({
+        id: id,
+        title: title
+      });
+      wx.setStorage({
+        key: 'historyVideo',
+        data: historyArray,
+        success:function() {
+          console.log("保存成功")
+        }
+      })
+    }
+    // console.log(id)
+    // console.log(event)
     wx.navigateTo({
       url: '/pages/videoDetail/videoDetail?type=movie&id=' + id,
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onClearHistory: function() {
+    var that = this
+    wx.removeStorage({
+      key: 'historyVideo',
+      success: function(res) {
+        console.log("删除成功")
+      },
+    })
+    that.setData({
+      historyArray: null
+    })
   }
+ 
+ 
+
 })
